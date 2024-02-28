2ac7db40644fc72072da61f6297da38be8b24724
A1aaSayed <alaa02.sayed02@gmail.com>
2024-02-28 23:25:10 +0200

    tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,13 +10,10 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	if (!parent)
-		return (NULL);
-
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 	binary_tree_t *temp;
 
-	if (!node)
+	if (!parent || !node)
 		return (NULL);
 
 	node->parent = parent;
